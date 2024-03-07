645d5cd4b2477646e707ac352f52babe03d7a005
Rafael <rafaeluliana2017@gmail.com>
2024-03-07 19:36:48 -0300

    intro concluída

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './Header'
+import Intro from './Intro'
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Intro />
     </div>
   )
 }
